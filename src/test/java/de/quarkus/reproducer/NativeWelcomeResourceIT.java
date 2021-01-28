c8f8c388dabb62f03e53ba6596a7957bc0124d21
Harald Reinmueller (FG-4-I-1) <Harald.RA.Reinmueller@bmwgroup.com>
2021-01-28 10:44:20 +0100

    cleanup

--- src/test/java/de/quarkus/reproducer/NativeWelcomeResourceIT.java
+++
@@ -1,9 +0,0 @@
-package de.quarkus.reproducer;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeWelcomeResourceIT extends WelcomeResourceTest {
-
-    // Execute the same tests but in native mode.
-}
